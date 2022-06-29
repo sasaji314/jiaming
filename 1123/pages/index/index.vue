eeac74bb7f37935663dfcb75e12153881a8d6518
ssj <2134@qq.com>
2022-06-29 18:29:44 +0800

    主页面、我的等页面

---
+++ 1123/pages/index/index.vue
@@ -0,0 +1,104 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<view class="box" hover-class="hoverclass">
+			ceshi
+			
+		</view>
+		<uni-search-bar @confirm="search" :focus="true" v-model="searchValue" @blur="blur" @focus="focus" @input="input"
+						@cancel="cancel" @clear="clear">
+		</uni-search-bar>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			toUser(){
+				uni.navigateTo({
+					url:'../user/user'
+				})
+			},
+			
+			search(res) {
+				uni.showToast({
+					title: '搜索：' + res.value,
+					icon: 'none'
+				})
+			},
+			input(res) {
+				console.log('----input:', res)
+			},
+			clear(res) {
+				uni.showToast({
+					title: 'clear事件，清除值为：' + res.value,
+					icon: 'none'
+				})
+			},
+			blur(res) {
+				uni.showToast({
+					title: 'blur事件，输入值为：' + res.value,
+					icon: 'none'
+				})
+			},
+			focus(e) {
+				uni.showToast({
+					itle: 'focus事件，输出值为：' + e.value,
+					icon: 'none'
+				})
+			},
+			cancel(res) {
+				uni.showToast({
+					title: '点击取消，输入值为：' + res.value,
+					icon: 'none'
+				})
+			},
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	.box{
+		border: 1px red solid;
+	}
+	.hoverclass {
+		background-color: aqua;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
---
+++ 1123/pages/user/vip/vip.vue
@@ -0,0 +1,132 @@
+<template>
+	<view>
+		<view class="main">
+			<view class="title">开通vip</view>
+			<view class="detail">现在加入即可享受查看商品和商店详情等服务</view>
+			
+			<view class="info">
+				
+				<uni-row class="demo-uni-row" :width="nvueWidth">
+					<uni-col :span="8">
+						<view class="left">
+							<view class="LT">
+								<text style="color: gainsboro;">超值优惠\n</text>
+								<text style="font-size: 250%;">12\n</text>
+								<text style="color: gainsboro;">个月</text>
+							</view>
+							<view class="LM">
+								<text style="border-top: goldenrod solid 1px;color: gainsboro;">10元 / 月\n</text>
+								<text style="color: goldenrod;">节省%67\n</text>
+							</view>
+							<view class="LB">
+								<text style="border-top: goldenrod solid 1px;font-size: 150%;">￥120</text>
+							</view>
+						</view>
+					</uni-col>
+					<uni-col :span="8">
+						<view class="mid">
+							<view class="MT">
+								<text style="color: gainsboro;">超值优惠\n</text>
+								<text style="font-size: 250%;">6\n</text>
+								<text style="color: gainsboro;">个月</text>
+							</view>
+							<view class="MM">
+								<text style="border-top: goldenrod solid 1px;color: gainsboro;">15元 / 月\n</text>
+								<text style="color: goldenrod;">节省%50\n</text>
+							</view>
+							<view class="MB">
+								<text style="border-top: goldenrod solid 1px;font-size: 150%;">￥90</text>
+							</view>
+						</view>
+					</uni-col>
+					<uni-col :span="8">
+						<view class="right">
+							<view class="RT">
+								<text style="color: gainsboro;">超值优惠\n</text>
+								<text style="font-size: 250%;">1\n</text>
+								<text style="color: gainsboro;">个月</text>
+							</view>
+							<view class="RM">
+								<text style="border-top: goldenrod solid 1px;color: gainsboro;">30元 / 月\n\n</text>
+							</view>
+							<view class="RB">
+								<text style="border-top: goldenrod solid 1px;font-size: 150%;">￥30</text>
+							</view>
+						</view>
+					</uni-col>
+				</uni-row>
+			</view>
+			<view class="button">
+				<button type="primary">确认开通</button>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	page{position: relative;background-color: black;}
+	.main{
+		width: 92%;
+		margin: 8% 4% 0 4%;
+		padding: 2% 0 10% 0;
+		background-color: #fff;
+		text-align: center;
+		border-radius: 25px;
+		
+	}
+	
+	.title{
+		margin-top: 5%;
+		margin-bottom: 10%;
+		
+		font-size: 200%;
+		color:goldenrod;
+	}
+	.detail{
+		margin-top: 5%;
+		padding-top: 5%;
+		font-size: 100%;
+	}
+	
+	.info{
+		padding-top:25% ;
+	}
+	
+	.LM{
+		padding-top: 10%;	
+	}
+	.LB{
+		padding-top: 10%;
+	}
+	.MM{
+		padding-top: 10%;	
+	}
+	.MB{
+		padding-top: 10%;
+	}
+	.RM{
+		padding-top: 10%;	
+	}
+	.RB{
+		padding-top: 10%;
+	}
+	
+	.button{
+		padding: 10% 10% 0 10%;
+		border-radius: 25px;
+		color:#EB4621;
+	}
+</style>
